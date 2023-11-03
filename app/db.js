e37f7ba5db4db1226e6ecaffab67419b9abe5a5e
Riccardo Zanotelli <riccardo.zanotelli@gmail.com>
2023-11-03 20:03:41 +0100

    Fixed database

--- app/db.js
+++ app/db.js
@@ -10,28 +10,14 @@ function createDbConnection() {
 		if (error) {
 			return console.error(error.message);
 		}
-		checkIfExistTable("setup")
-		console.log("Connection with SQLite has been established");
-
+		onAfterConnect()
 	});
 	return db;
 }
 
-
-function checkIfExistDatabase() {
-	db.all(`SELECT name FROM sqlite_master WHERE type='table' AND name='${tableName}'`, (err, rows) => {
-		if(err) {
-			console.log(err)
-		} else {
-			onAfterConnect()
-		}
-	})
-}
-
 function onAfterConnect() {
-	db.run(`CREATE TABLE IF NOT EXISTS 'setup' ('version' INTEGER)`, (err, ret) => console.log(ret));
-	db.run(`CREATE TABLE IF NOT EXISTS 'reminder_pin' ('weekNr' INTEGER, 'chatId' INTEGER, 'msgId' INTEGER, PRIMARY KEY (weekNr))`, (err, ret) => console.log(ret));
-
+	db.run(`CREATE TABLE IF NOT EXISTS 'setup' ('version' INTEGER)`, (err, ret) => {});
+	db.run(`CREATE TABLE IF NOT EXISTS 'reminder_pin' ('weekNr' INTEGER, 'chatId' INTEGER, 'msgId' INTEGER, PRIMARY KEY (weekNr))`, (err, ret) => {});
 }
 
 
